33a196bf94f595593781fd5f2c7e4ede433694a6
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-11-23 15:42:12 +0100

    add festival list transitions

--- src/components/FestivalList.jsx
+++ src/components/FestivalList.jsx
@@ -4,6 +4,7 @@ import { useAtom, useAtomValue } from "jotai";
 import { highlightAtom } from "../atoms/highlightAtom";
 import { festivalsAtom } from "../atoms/festivalsAtom";
 import { mapFiltersAtom } from "../atoms/mapFiltersAtom";
+import { AnimatePresence, motion } from "motion/react";
 import { filterFestivals } from "../utils";
 
 export default function FestivalList() {
@@ -17,27 +18,34 @@ export default function FestivalList() {
       <h2 className="font-vk mt-2 border-b border-[salmon] p-1 text-center text-2xl font-bold text-[salmon] sm:text-3xl">
         Festivals in 2025
       </h2>
-      <ul className="font-vk flex flex-wrap items-start justify-start overflow-y-auto p-2 text-center">
-        {filterFestivals(festivals, filters, location)
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((f) => {
-            const hl = highlight === f.slug;
-            return (
-              <li
-                key={f.name}
-                className="w-full"
-                onMouseEnter={() => setHighlight(f.slug)}
-                onMouseLeave={() => setHighlight(null)}
-              >
-                <Link
-                  className={`text-center text-lg font-bold transition hover:text-[salmon] md:text-2xl ${hl ? "text-[salmon]" : ""}`}
-                  href={`/${f.slug}`}
+      <ul className="font-vk flex flex-wrap items-start justify-start overflow-y-scroll p-2 text-center">
+        <AnimatePresence>
+          {filterFestivals(festivals, filters, location)
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((f) => {
+              const hl = highlight === f.slug;
+              return (
+                <motion.li
+                  key={f.name}
+                  initial={{ opacity: 0, y: 5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -5 }}
+                  transition={{ duration: 0.3 }}
+                  layout
+                  className="w-full"
+                  onMouseEnter={() => setHighlight(f.slug)}
+                  onMouseLeave={() => setHighlight(null)}
                 >
-                  {f.name}
-                </Link>
-              </li>
-            );
-          })}
+                  <Link
+                    className={`text-center text-lg font-bold transition hover:text-[salmon] md:text-2xl ${hl ? "text-[salmon]" : ""}`}
+                    href={`/${f.slug}`}
+                  >
+                    {f.name}
+                  </Link>
+                </motion.li>
+              );
+            })}
+        </AnimatePresence>
       </ul>
     </>
   );
